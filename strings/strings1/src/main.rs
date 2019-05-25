8a19f5ecbab1aa0ff74a5bd7d47b99742f3c2928
Mikhail Chernyavsky <chemike47@gmail.com>
2019-05-26 00:15:58 +0300

    Strings

---
+++ strings/strings1/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let answer = current_favorite_color();
+    println!("My current favorite color is {}", answer);
+}
+
+fn current_favorite_color() -> String {
+    "blue"
+}
---
+++ strings/strings2/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let word = String::from("green"); // Try not changing this line :)
+    if is_a_color_word(word) {
+        println!("That is a color word I know!");
+    } else {
+        println!("That is not a color word I know.");
+    }
+}
+
+fn is_a_color_word(attempt: &str) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
