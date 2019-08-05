e0fa91522bc2e9c61915e75c267864ad4297c192
Geravant <igor.gera.gerasim@gmail.com>
2019-08-05 19:47:40 +0300

    Storing Values with Variables - test checking the error if input is absent is implemented

--- Guessing Game/Programming a Guessing Game/Storing Values with Variables/tests/tests.rs
+++ Guessing Game/Programming a Guessing Game/Storing Values with Variables/tests/tests.rs
@@ -1,7 +1,7 @@
 extern crate escargot;
 
 use std::panic::{PanicInfo};
-use std::fmt;
+use std::{fmt, ptr};
 use std::io::{BufWriter, Write, Read, BufReader};
 use std::process::Stdio;
 use std::borrow::Borrow;
@@ -9,15 +9,11 @@ use std::borrow::Borrow;
 fn report_students_error(info: &std::panic::PanicInfo) {
     let mut assert_output = StudentError::new(String::from(""));
     match info.payload().downcast_ref::<&str>() {
-        Some(msg) => {println!("{:?}", msg)}
+        Some(msg) => {println!("{}", msg)}
         None => {
             assert_output.set_msg(info);
             println!("{}", assert_output.get_msg());
         }
-    }
-
-    if let Some(msg) = info.payload().downcast_ref::<&str>() {
-        println!("The error message was: {}", msg);
     }
 }
 
@@ -104,50 +100,58 @@ fn prints_intro_text_and_guesses_a_number100() {
     //TODO: recover from an incorrect output
     let actual_as_string = &actual_output;
     let expected  = "Guess the number!\nPlease input your guess.\nYou guessed: 100\n";
+    let err_only_two_lines_expected = "Guess the number!\nPlease input your guess.\n";
+    let err_intro_line_missed = "Please input your guess.\n";
     std::panic::set_hook(Box::new(|panic_info|
         report_students_error(panic_info)
     ));
+    assert_ne!(actual_as_string, err_only_two_lines_expected, "Note, that you should print what \
+    user guessed.\n");
+    assert_ne!(actual_as_string, err_intro_line_missed, "Note, that you should introduce the game \
+    first\n");
     assert_eq!(actual_as_string, expected, "The output for input \"100\" was incorrect:\n");
     std::panic::take_hook();
 }
 
 #[test]
-fn prints_fail_text_if_no_input() {
+fn prints_error_if_failed_to_read_input() {
     //TODO: here are even two unwraps in a row, that's dangerous
     let actual_command = escargot::CargoBuild::new()
         .bin("processing_a_guess_2")
         .run()
         .unwrap()
         .command()
-//        .stdin(Stdio::piped())
-//        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn();
 
-    let mut actual = match actual_command {
+    let actual = match actual_command {
         //TODO: Change the message
         Err(why) => panic!("couldn't start main.rs: {}", why),
         Ok(process) => process,
     };
 
-    let mut result = match actual.wait_with_output() {
-        //TODO: Change the message
-        Err(why) => panic!("failed!: {}", why),
-        Ok(process) => process,
-    };
-
-    let mut actual_output;
-    match std::str::from_utf8(result.stdout.as_slice()) {
+    let zero_bytes : &[u8] =  &[0xAC];
+    match actual.stdin.unwrap().write_all(zero_bytes) {
         //TODO: process this message, it would be uninformative for the student
-        Err(why) => panic!("couldn't read stdout: {}",
+        Err(why) => panic!("couldn't write to stdin: {}",
                            why),
-        Ok(output) => {actual_output = output},
+        Ok(_) => {},
     }
-    //TODO: recover from an incorrect output
-    let expected  = "Guess the number!\nPlease input your guess.\nYou guessed: 100\n";
+    let mut actual_error = String::new();
+    match actual.stderr.unwrap().read_to_string(&mut actual_error) {
+        //TODO: process this message, it would be uninformative for the student
+        Err(why) => panic!("couldn't read stderr: {}",
+                           why),
+        Ok(_) => {},
+    }
+    //TODO: recover from an incorrect error
+    let expected  = "Failed to read line";
     std::panic::set_hook(Box::new(|panic_info|
         report_students_error(panic_info)
     ));
-    assert_eq!(actual_output, expected, "The output for input \"100\" was incorrect:\n");
+    assert!(actual_error.contains(expected), "The output in case of an absent input was incorrect.\n The program should return \"Failed to read line\".");
+//    assert_eq!(actual_error, expected, "The output in case of an absent input was incorrect:\n");
     std::panic::take_hook();
 }
 
@@ -185,7 +189,7 @@ impl<'a> StudentError<String> {
         let message_starts =  panic_to_str.find("message: Some").unwrap()+51;
         let message_ends =  panic_to_str.find("location: Location").unwrap()-3;
         let header_starts = panic_to_str.find("`: ").unwrap()+3;
-        let mut header = String::from(&panic_to_str[header_starts..message_ends]);
+        let header = String::from(&panic_to_str[header_starts..message_ends]);
         //TODO: another magic number: -2 is for ": " inserted after expected message
         let mut result = String::from(&panic_to_str[message_starts..header_starts-2]);
         result = result.replace("left: `", "actual: ");
