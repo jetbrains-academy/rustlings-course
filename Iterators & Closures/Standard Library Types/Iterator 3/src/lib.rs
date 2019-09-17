73e8ed8afff6069b64daa2504c541ed549e9c443
Geravant <igor.gera.gerasim@gmail.com>
2019-09-17 15:15:45 +0300

    Iterators & Closures - Standard Library Types - Iterator 3: type switched to edu, placeholder added, test implemented

--- Iterators & Closures/Standard Library Types/Iterator 3/src/lib.rs
+++ Iterators & Closures/Standard Library Types/Iterator 3/src/lib.rs
@@ -6,14 +6,22 @@ pub enum DivisionError {
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct NotDivisibleError {
-    dividend: i32,
-    divisor: i32,
+    pub dividend: i32,
+    pub divisor: i32,
 }
 
 // This function should calculate `a` divided by `b` if `a` is evenly divisible by b.
 // Otherwise, it should return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    }
+    else if a % b == 0 {
+        Ok(a/b)
+    }
+    else {
+        Err(DivisionError::NotDivisible(NotDivisibleError{ dividend: a, divisor: b }))
+    }
 }
 
 #[cfg(test)]
@@ -26,14 +34,14 @@ mod tests {
     fn result_with_list() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x= division_results.collect::<Result<Vec<_>, _>>();
         assert_eq!(format!("{:?}", x), "Ok([1, 11, 1426, 3])");
     }
     #[test]
     fn list_of_results() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = division_results.collect::<Vec<Result<i32, _>>>();
         assert_eq!(format!("{:?}", x), "[Ok(1), Ok(11), Ok(1426), Ok(3)]");
     }
 }
