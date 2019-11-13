c2c9070be09e87517e304ea9e5de4c6867d26183
Geravant <igor.gera.gerasim@gmail.com>
2019-11-13 18:12:20 +0300

    refactor(Course): hooks in tests removed
    The report_student_error function, its panic hooks and the
    infrastructure removed from all of the tests
    
    BREAKING CHANGE: below 3.2

--- Common Collections/String Test/Strings and strs/tests/tests.rs
+++ Common Collections/String Test/Strings and strs/tests/tests.rs
@@ -1,6 +1,3 @@
-use std::fmt;
-use std::panic::PanicInfo;
-
 #[test]
 fn prints_the_second_number_is() {
     //TODO: here are even two unwraps in a row, that's dangerous
@@ -25,9 +22,6 @@ fn prints_the_second_number_is() {
     let expected_happy_monday = "Happy Tuesday!";
     let expected_my_shift_key_is_sticky = "my shift key is sticky";
 
-    std::panic::set_hook(Box::new(|panic_info|
-        report_students_error(panic_info)
-    ));
     assert!(actual_as_string.contains(expected_blue), "The output is missing the line \"blue\"\n");
     assert!(actual_as_string.contains(expected_red), "The output is missing the line \"red\"\n");
     assert!(actual_as_string.contains(expected_hi), "The output is missing the line \"hi\"\n");
@@ -38,77 +32,4 @@ fn prints_the_second_number_is() {
     assert!(actual_as_string.contains(expected_hello_there), "The output is missing the line \"hello there\"\n");
     assert!(actual_as_string.contains(expected_happy_monday), "The output is missing the line \"Happy Monday!\"\n");
     assert!(actual_as_string.contains(expected_my_shift_key_is_sticky), "The output is missing the line \"my shift key is sticky\"\n");
-    std::panic::take_hook();
-}
-
-
-fn report_students_error(info: &std::panic::PanicInfo) {
-    let mut assert_output = StudentError::new(String::from(""));
-    match info.payload().downcast_ref::<&str>() {
-        Some(msg) => {println!("{:?}", msg)}
-        None => {
-            assert_output.set_msg(info);
-            println!("{}", assert_output.get_msg());
-        }
-    }
-
-    if let Some(msg) = info.payload().downcast_ref::<&str>() {
-        println!("The error message was: {}", msg);
-    }
-}
-
-struct StudentError<String> {
-    msg: String
-}
-
-impl<'a> std::fmt::Display for StudentError<String> {
-    fn fmt(&self, f: & mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.msg)?;
-        Ok(())
-    }
-}
-
-impl<'a> StudentError<String> {
-    fn new(msg: String) -> Self{
-        StudentError {
-            msg
-        }
-    }
-    fn set_msg(&mut self, info: &PanicInfo<'a>) {
-        self.update_msg_with_panic_info(info);
-    }
-
-    fn get_msg(self) -> String{
-        return self.msg
-    }
-
-    //TODO: get rid of all of this and implement smth mature
-    fn update_msg_with_panic_info(&mut self, info: &PanicInfo<'a>) {
-        let panic_to_str = &format!("{:?}", info);
-        //TODO: get rid of magic numbers 51 is for the message: Some.. template and -3 is for
-        // \n and whitespace before location
-        let expected_error = panic_to_str.find("`(left == right)`");
-        let message_starts =  panic_to_str.find("message: Some").unwrap()+51;
-        let message_ends =  panic_to_str.find("location: Location").unwrap()-3;
-        let header_starts = panic_to_str.find("`: ").unwrap()+3;
-        let mut header = String::from((&panic_to_str[header_starts..message_ends]));
-        let mut result = String::from(&panic_to_str[message_starts..header_starts]);
-        result = result.replace("left: `", "actual: ");
-        match expected_error {
-            Some(x) =>
-                result = result.replace("`,\n right: `", "\n expected: "),
-            None =>
-                result = result.replace("`,\n right: `", "\n predicted for this error: ")
-        }
-        result = result.replace("`", "");
-        self.msg += &header;
-        self.msg += "\n";
-        self.msg += &result;
-    }
-}
-
-impl Clone for StudentError<String> {
-    fn clone(&self) -> Self {
-        Self::new(self.msg.clone())
-    }
 }
