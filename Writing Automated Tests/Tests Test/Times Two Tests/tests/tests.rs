e9cf9a6be3887c8fc8280605ac507e70e9785155
Geravant <igor.gera.gerasim@gmail.com>
2019-11-29 10:50:24 +0300

    fix(WAT - Test Test / Testing task): Learners Tests moved to tests folder
    LEarner tests moved to the test golder to allow checking

--- Writing Automated Tests/Tests Test/Times Two Tests/tests/tests.rs
+++ Writing Automated Tests/Tests Test/Times Two Tests/tests/tests.rs
@@ -1,7 +1,16 @@
-#[test]
-fn if_compiles() {
-    //Mocking test to run only the compiler's checker
-    assert!(true);
+#[cfg(test)]
+mod tests {
+    use times_two_tests::times_two;
+
+    #[test]
+    fn returns_twice_of_positive_numbers() {
+        assert_eq!(times_two(2), 4);
+    }
+
+    #[test]
+    fn returns_twice_of_negative_numbers() {
+        assert_eq!(times_two(-2), -4);
+    }
 }
 
 
--- Writing Automated Tests/Tests/Testing Task/tests/tests.rs
+++ Writing Automated Tests/Tests/Testing Task/tests/tests.rs
@@ -1,7 +1,16 @@
-#[test]
-fn if_compiles() {
-    //Mocking test to run only the compiler's checker
-    assert!(true);
+#[cfg(test)]
+mod tests {
+    use testing_task::is_even;
+
+    #[test]
+    fn is_true_when_even() {
+        assert!(is_even(2));
+    }
+
+    #[test]
+    fn is_false_when_odd() {
+        assert_eq!(is_even(5), false);
+    }
 }
 
 
