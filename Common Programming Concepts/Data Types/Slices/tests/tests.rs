7365311a1b8fd3b6565a4583e388860968b4315c
Geravant <igor.gera.gerasim@gmail.com>
2020-01-28 18:44:13 +0300

    fix(Common Programming Concepts - Data Types - Slices): Test fixed to perform the correctness check
    Test modified to actually run the solution and perform the output check
    for the correct branch message to appear.
    
    Closes https://github.com/jetbrains-academy/rustlings-course/issues/24

--- Common Programming Concepts/Data Types/Slices/tests/tests.rs
+++ Common Programming Concepts/Data Types/Slices/tests/tests.rs
@@ -1,7 +1,20 @@
+extern crate escargot;
+
 #[test]
 fn if_compiles() {
     //Mocking test to run only the compiler's checker
-    assert!(true);
+    let actual = escargot::CargoBuild::new()
+        .bin("slices")
+        .run()
+        .unwrap()
+        .command()
+        .output()
+        .unwrap()
+        .stdout;
+    //TODO: recover from an incorrect output
+    let actual_as_string = std::str::from_utf8(&actual).unwrap();
+    let expected_true  = "Nice slice!";
+    assert!(actual_as_string.contains(expected_true), "The output should be \"Nice slice!\". Check, if the statement returns true.");
 }
 
 
