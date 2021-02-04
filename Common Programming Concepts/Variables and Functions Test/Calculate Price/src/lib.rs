c4d68dadc393955a7d36c837a2ceac1337baa37a
sofiiako <19rubisco93@gmail.com>
2021-02-04 17:19:39 +0300

    Updated the task (changed the solution, edited placeholder)

--- Common Programming Concepts/Variables and Functions Test/Calculate Price/src/lib.rs
+++ Common Programming Concepts/Variables and Functions Test/Calculate Price/src/lib.rs
@@ -1,9 +1,7 @@
-use std::cmp::Ordering;
-
 pub fn calculateprice(order_amount: i32) -> i32 {
-    match order_amount.cmp(&40) {
-        Ordering::Less => order_amount*2,
-        Ordering::Equal => order_amount*2,
-        Ordering::Greater => order_amount
+    if order_amount > 40 {
+        return order_amount
+    } else {
+        return order_amount * 2
     }
 }
