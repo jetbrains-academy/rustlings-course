7cb0f6b335837b1f97a0dece5c7f72ca714b0676
sofiiako <19rubisco93@gmail.com>
2021-02-26 00:14:07 +0300

    Added task code to "Initiate Struct"; added tests

--- Structs/Structs/Initiate Struct/src/main.rs
+++ Structs/Structs/Initiate Struct/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    // put you code here to launch it
+    let x = classic_c_structs();
+    println!("{:?}", x);
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+struct ColorClassicStruct {
+    name: String,
+    hex: String,
+}
+
+struct ColorTupleStruct(String, String);
+
+#[derive(Debug)]
+struct UnitStruct;
+
+fn classic_c_structs() -> ColorClassicStruct {
+    let green = ColorClassicStruct{
+        name: String::from("green"),
+        hex: String::from("#00FF00"),
+    };
+    return green
+}
+
+fn tuple_structs() -> ColorTupleStruct {
+    let green = ColorTupleStruct(String::from("green"), String::from("#00FF00"));
+    return green
+}
+
+fn unit_structs() -> String {
+    let unit_struct = UnitStruct;
+    let message = format!("{:?}s are fun!", unit_struct);
+    return message
 }
