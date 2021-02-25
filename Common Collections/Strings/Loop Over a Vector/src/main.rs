e5c0985e709a5ece780192cb063c5a03087ec1d0
sofiiako <19rubisco93@gmail.com>
2021-02-25 20:54:26 +0300

    Added a proper test to "Loop over a vector"; edited the task code

--- Common Collections/Strings/Loop Over a Vector/src/main.rs
+++ Common Collections/Strings/Loop Over a Vector/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-    fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-        for i in v.iter_mut() {
-            *i *= 2;
-        }
-        // At this point, `v` should be equal to [4, 8, 12, 16, 20].
-        v
-    }
+    let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+    let ans = vec_loop(v.clone());
+    println!("Vector: {:?}", ans);
 }
+
+fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
+    for i in v.iter_mut() {
+        *i *= 2;
+    }
+        // At this point, `v` should be equal to [4, 8, 12, 16, 20].
+    v
+}
+
 
 
--- Common Collections/Strings/Loop Over a Vector/tests/tests.rs
+++ Common Collections/Strings/Loop Over a Vector/tests/tests.rs
@@ -1,5 +1,15 @@
+use loop_over_a_vector::vec_loop;
+
+
 #[test]
-fn if_compiles() {
-    //Mocking test to run only the compiler's checker
-    assert!(true);
+fn test_vec_loop() {
+    let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+    let ans = vec_loop(v.clone());
+
+    assert_eq!(
+        ans,
+        v.iter()
+            .map(|x| x * 2)
+            .collect::<Vec<i32>>()
+    );
 }
