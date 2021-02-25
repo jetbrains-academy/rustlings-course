4ae5b46cb7914d2e1a3553881ba3280237aeec34
sofiiako <19rubisco93@gmail.com>
2021-02-25 21:03:01 +0300

    Added a proper test to "Declarind a Hashmap"; edited the task code (took fruit_basket out of main, added println to main)

--- Common Collections/Strings/Declaring a HashMap/tests/tests.rs
+++ Common Collections/Strings/Declaring a HashMap/tests/tests.rs
@@ -1,5 +1,15 @@
+use declaring_a_hashmap::fruit_basket;
+
 #[test]
-fn if_compiles() {
-    //Mocking test to run only the compiler's checker
-    assert!(true);
+fn at_least_three_types_of_fruits() {
+    let basket = fruit_basket();
+    assert!(basket.len() >= 3);
+}
+
+#[test]
+fn at_least_five_fruits() {
+    let basket = fruit_basket();
+    assert!(basket
+        .values()
+        .sum::<u32>() >= 5);
 }
--- Common Collections/Strings/Loop Over a Vector/tests/tests.rs
+++ Common Collections/Strings/Loop Over a Vector/tests/tests.rs
@@ -1,6 +1,5 @@
 use loop_over_a_vector::vec_loop;
 
-
 #[test]
 fn test_vec_loop() {
     let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
