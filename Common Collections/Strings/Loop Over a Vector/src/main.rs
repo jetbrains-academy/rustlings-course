d1b5fa221be669ccf824b2ad422c9827dd3b16a4
sofiiako <19rubisco93@gmail.com>
2021-02-26 18:43:25 +0300

    Fixed the tests in "Loop over a Vector"

--- Common Collections/Strings/Loop Over a Vector/src/main.rs
+++ Common Collections/Strings/Loop Over a Vector/src/main.rs
@@ -1,16 +1,12 @@
+use loop_over_a_vector::vec_loop;
+
 fn main() {
     let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
     let ans = vec_loop(v.clone());
     println!("Vector: {:?}", ans);
 }
 
-fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
-        *i *= 2;
-    }
-        // At this point, `v` should be equal to [4, 8, 12, 16, 20].
-    v
-}
+
 
 
 
