5344124e3b8d75f606b6dfd973a1e3b79c02da2e
sofiiako <19rubisco93@gmail.com>
2021-02-26 18:40:50 +0300

    Fixed the tests in "Declaring a Vector"

--- Common Collections/Strings/Declaring a Vector/src/main.rs
+++ Common Collections/Strings/Declaring a Vector/src/main.rs
@@ -1,15 +1,8 @@
+use declaring_a_vector::array_and_vec;
+
 fn main() {
     let (a, v) = array_and_vec();
     println!("Array: {:?}", a);
     println!("Vector: {:?}", v);
 }
 
-fn array_and_vec() -> ([i32; 4], Vec<i32>) {
-    let a = [10, 20, 30, 40]; // a plain array
-    let v = a.to_vec();
-
-    (a, v)
-}
-
-//fn array_and_vec() {
-//}
