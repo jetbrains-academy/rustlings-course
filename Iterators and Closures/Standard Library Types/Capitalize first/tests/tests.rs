e353ea5570712cecfa7594e62802a1dfcd13b052
sofiiako <19rubisco93@gmail.com>
2021-03-22 17:26:04 +0300

    Added Capitalize First exercise (iterators2)

---
+++ Iterators and Closures/Standard Library Types/Capitalize first/tests/tests.rs
@@ -0,0 +1,26 @@
+use capitalize_first::*;
+
+#[test]
+fn test_success() {
+    assert_eq!(capitalize_first("hello"), "Hello");
+}
+
+#[test]
+fn test_empty() {
+    assert_eq!(capitalize_first(""), "");
+}
+
+// Step 2.
+#[test]
+fn test_iterate_string_vec() {
+    let words = vec!["hello", "world"];
+    let capitalized_words: Vec<String> = vec![capitalize_first(words[0]), capitalize_first(words[1])];
+        assert_eq!(capitalized_words, ["Hello", "World"]);
+}
+
+#[test]
+fn test_iterate_into_string() {
+    let words = vec!["hello", " ", "world"];
+    let capitalized_words = words.into_iter().map(capitalize_first).collect::<Vec<String>>().join("");
+        assert_eq!(capitalized_words, "Hello World");
+}
