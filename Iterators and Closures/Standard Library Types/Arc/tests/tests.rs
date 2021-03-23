78f998d5ee425930d75a2e8d06714677bb52616f
sofiiako <19rubisco93@gmail.com>
2021-03-23 16:42:25 +0300

    Fixed the test in Arc

--- Iterators and Closures/Standard Library Types/Arc/tests/tests.rs
+++ Iterators and Closures/Standard Library Types/Arc/tests/tests.rs
@@ -1,8 +1,5 @@
-use std::{thread};
-use std::thread::Thread;
-use std::time::Duration;
 use std::process::Stdio;
-use std::io::{BufReader, BufRead, Read};
+use std::io::{BufReader, Read};
 
 #[test]
 fn prints_the_vector_from_fill_vec() {
@@ -15,7 +12,7 @@ fn prints_the_vector_from_fill_vec() {
         .stdout(Stdio::piped())
         .spawn();
 
-    let mut actual = match actual_command {
+    let actual = match actual_command {
         //TODO: Change the message
         Err(why) => panic!("couldn't start main.rs: {}", why),
         Ok(process) => process,
@@ -25,15 +22,15 @@ fn prints_the_vector_from_fill_vec() {
     //Creating the buffer
     let mut actual_stdout = BufReader::new(actual.stdout.unwrap());
     let mut actual_as_string = String::new();
-    actual_stdout.read_to_string(&mut actual_as_string);
-    let expected_sum_of_offset_0  = "Sum of offset 0 is 950";
-    let expected_sum_of_offset_1  = "Sum of offset 1 is 970";
-    let expected_sum_of_offset_2  = "Sum of offset 2 is 990";
-    let expected_sum_of_offset_3  = "Sum of offset 3 is 1010";
-    let expected_sum_of_offset_4  = "Sum of offset 4 is 1030";
-    let expected_sum_of_offset_5  = "Sum of offset 5 is 950";
-    let expected_sum_of_offset_6  = "Sum of offset 6 is 969";
-    let expected_sum_of_offset_7  = "Sum of offset 7 is 988";
+    actual_stdout.read_to_string(&mut actual_as_string).ok();
+    let expected_sum_of_offset_0  = "Sum of offset 0 is 624";
+    let expected_sum_of_offset_1  = "Sum of offset 1 is 637";
+    let expected_sum_of_offset_2  = "Sum of offset 2 is 650";
+    let expected_sum_of_offset_3  = "Sum of offset 3 is 663";
+    let expected_sum_of_offset_4  = "Sum of offset 4 is 576";
+    let expected_sum_of_offset_5  = "Sum of offset 5 is 588";
+    let expected_sum_of_offset_6  = "Sum of offset 6 is 600";
+    let expected_sum_of_offset_7  = "Sum of offset 7 is 612";
     assert!(actual_as_string.contains(expected_sum_of_offset_0), "There was not a sum of the offset 0, or it was incorrect");
     assert!(actual_as_string.contains(expected_sum_of_offset_1), "There was not a sum of the offset 1, or it was incorrect");
     assert!(actual_as_string.contains(expected_sum_of_offset_2), "There was not a sum of the offset 2, or it was incorrect");
