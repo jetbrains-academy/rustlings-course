6c64b79477eb2d2c4df5deefe723dbbf8db96e49
sofiiako <19rubisco93@gmail.com>
2021-05-13 15:50:49 +0300

    Added comments with old solution in FromStr

--- Type Conversions/Type Conversions/FromStr/src/lib.rs
+++ Type Conversions/Type Conversions/FromStr/src/lib.rs
@@ -13,24 +13,24 @@ impl FromStr for Person {
 
         if s.len() == 0 {
             return Err(Box::new("empty string cannot be parsed into Person"));
-            // return Err(Box::new(String::from("empty string cannot be parsed into Person")));
+            // return Err(String::from("empty string cannot be parsed into Person"));
         }
         let parts: Vec<&str> = s.split(',').collect();
         if (parts.len() < 2) | (parts.len() > 2) {
             return Err(Box::new("must contain 2 fields exactly: name and age"));
-            //return Err(Box::new(String::from("must contain 2 fields exactly: name and age")));
+            //return Err(String::from("must contain 2 fields exactly: name and age"));
         }
         let name = if parts[0] != "" {
             String::from(parts[0])
         } else {
             return Err(Box::new("name field must not be empty"));
-            //return Err(Box::new(String::from("name field must not be empty")));
+            //return Err(String::from("name field must not be empty"));
         };
         if let Ok(age) = parts[1].parse::<usize>() {
             return Ok(Person {name, age});
         } else {
             return Err(Box::new("cannot parse age"));
-            //return Err(Box::new(String::from("cannot parse age")));
+            //return Err(String::from("cannot parse age"));
         };
     }
 }
