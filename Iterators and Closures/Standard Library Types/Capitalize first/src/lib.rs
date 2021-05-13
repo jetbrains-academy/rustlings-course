8ba9be5342ab897148d244e62736890c9d9616b1
sofiiako <19rubisco93@gmail.com>
2021-05-13 18:04:18 +0300

    Added working solution to Capitalize First

--- Iterators and Closures/Standard Library Types/Capitalize first/src/lib.rs
+++ Iterators and Closures/Standard Library Types/Capitalize first/src/lib.rs
@@ -8,28 +8,26 @@ pub fn capitalize_first(input: &str) -> String {
 
 // Step 2.
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    return words.iter().map(|word | capitalize_first(word)).collect::<Vec<String>>();
+}
+
+// This function is only needed to test your code through main.rs
+pub fn iterate_string_vec() {
+     let words = vec!["hello", "world"];
+     let capitalized_words: Vec<String> = vec![capitalize_first(words[0]), capitalize_first(words[1])];
+     println!("{:?}", capitalized_words);
 }
 
 // Step 3.
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    return words.iter().map(|word | capitalize_first(word)).collect::<String>();
 }
 
-
-
-// // Step 2
-// pub fn iterate_string_vec() {
-//     let words = vec!["hello", "world"];
-//     let capitalized_words: Vec<String> = vec![capitalize_first(words[0]), capitalize_first(words[1])];
-//     println!("{:?}", capitalized_words);
-// }
-//
-// // Step 3
-// pub fn iterate_into_string() {
-//     let words = vec!["hello", " ", "world"];
-//     let capitalized_words = words.into_iter().map(capitalize_first).collect::<Vec<String>>().join("");
-//     println!("{:?}", capitalized_words);
-// }
+// This function is only needed to test your code through main.rs
+pub fn iterate_into_string() {
+     let words = vec!["hello", " ", "world"];
+     let capitalized_words = words.into_iter().map(capitalize_first).collect::<Vec<String>>().join("");
+     println!("{:?}", capitalized_words);
+}
 
 
