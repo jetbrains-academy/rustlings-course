ba98d5e9469e529afff6729ebfee3c60e4f95b83
sofiiako <19rubisco93@gmail.com>
2021-05-13 14:40:05 +0300

    Updated test in Loop Over a Vector

--- Common Collections/Strings/Loop Over a Vector/tests/tests.rs
+++ Common Collections/Strings/Loop Over a Vector/tests/tests.rs
@@ -5,10 +5,5 @@ fn test_vec_loop() {
     let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
     let ans = vec_loop(v.clone());
 
-    assert_eq!(
-        ans,
-        v.iter()
-            .map(|x| x * 2)
-            .collect::<Vec<i32>>()
-    );
+    assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
 }
