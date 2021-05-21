4acabea0a3289b0d523fe72710bb1bfa9a1106c9
sofiiako <19rubisco93@gmail.com>
2021-05-21 17:04:19 +0300

    Removed commented lines in capitalize first tests

--- Iterators and Closures/Standard Library Types/Capitalize first/tests/tests.rs
+++ Iterators and Closures/Standard Library Types/Capitalize first/tests/tests.rs
@@ -15,14 +15,10 @@ fn test_empty() {
 fn test_iterate_string_vec() {
     let words = vec!["hello", "world"];
     assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
-    // let capitalized_words: Vec<String> = vec![capitalize_first(words[0]), capitalize_first(words[1])];
-    //     assert_eq!(capitalized_words, ["Hello", "World"]);
 }
 
 #[test]
 fn test_iterate_into_string() {
     let words = vec!["hello", " ", "world"];
     assert_eq!(capitalize_words_string(&words), "Hello World");
-    // let capitalized_words = words.into_iter().map(capitalize_first).collect::<Vec<String>>().join("");
-    //     assert_eq!(capitalized_words, "Hello World");
 }
