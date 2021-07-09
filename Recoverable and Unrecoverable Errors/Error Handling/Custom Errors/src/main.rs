78396e8f400522a9b798a0297a6feac9887dbf34
sofiiako <19rubisco93@gmail.com>
2021-07-09 20:32:14 +0300

    Added Custom Errors, only partial solution for now

--- Recoverable and Unrecoverable Errors/Error Handling/Custom Errors/src/main.rs
+++ Recoverable and Unrecoverable Errors/Error Handling/Custom Errors/src/main.rs
@@ -1,7 +1,7 @@
 use errors6::*;
 
 fn main() {
-    //println!("{:?}", parse_pos_nonzero("not a number"));
+    println!("{:?}", parse_pos_nonzero("not a number"));
     println!("{:?}", parse_pos_nonzero("-555"));
     println!("{:?}", parse_pos_nonzero("0"));
     println!("{:?}", parse_pos_nonzero("42"));
