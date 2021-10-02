5ab844ee000513a53c6bd736d1a0c043ff08453e
sofiiako <19rubisco93@gmail.com>
2021-10-03 00:43:27 +0300

    Updated task Public Modifier (modules 1) https://github.com/rust-lang/rustlings/commit/dfd2fab4f33d1bf59e2e5ee03123c0c9a67a9481

--- Modules and Macros/Modules/Public Modifier/src/main.rs
+++ Modules and Macros/Modules/Public Modifier/src/main.rs
@@ -1,5 +1,10 @@
 mod sausage_factory {
+    // Don't let anybody outside of this module see this!
+    fn get_secret_recipe() -> String {
+        String::from("Ginger")
+    }
     pub fn make_sausage() {
+        get_secret_recipe();
         println!("sausage!");
     }
 }
