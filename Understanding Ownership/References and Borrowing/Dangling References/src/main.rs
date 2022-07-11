af60f649f609800512adad0288c72824b155984b
Vitaly Bragilevsky <bravit111@gmail.com>
2022-07-11 15:07:19 +0300

    Add comments about errors in the code

--- Understanding Ownership/References and Borrowing/Dangling References/src/main.rs
+++ Understanding Ownership/References and Borrowing/Dangling References/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let reference_to_nothing = dangle();
 }
 
+// !!! ERROR: returning reference for a local variable
 fn dangle() -> &String {
     let s = String::from("hello");
 
--- Understanding Ownership/What is ownership/Moving Ownership/src/main.rs
+++ Understanding Ownership/What is ownership/Moving Ownership/src/main.rs
@@ -2,5 +2,6 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1;
 
+    // !!! ERROR: value is moved to s2
     println!("{}, world!", s1);
 }
