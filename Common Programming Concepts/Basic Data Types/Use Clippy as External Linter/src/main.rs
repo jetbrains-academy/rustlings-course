83628c29c56f477db13e241fcbe8d9e9848ca994
bravit <bravit111@gmail.com>
2022-11-29 08:27:01 +0200

    add clippy1 from rustlings

---
+++ Common Programming Concepts/Basic Data Types/Use Clippy as External Linter/src/main.rs
@@ -0,0 +1,13 @@
+use std::f32;
+
+fn main() {
+    let pi = f32::consts::PI;
+    let radius = 5.00f32;
+
+    let area = pi * f32::powi(radius, 2);
+
+    println!(
+        "The area of a circle with radius {:.2} is {:.5}!",
+        radius, area
+    )
+}
