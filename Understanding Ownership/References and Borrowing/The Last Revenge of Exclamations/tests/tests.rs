8005ecb53326d841293ad7ee0007a70d40d697bf
tito <tito@serpstat.com>
2023-05-05 23:30:39 +0300

    fixed text so it can be displayed in idea correctly

--- Understanding Ownership/References and Borrowing/The Last Revenge of Exclamations/tests/tests.rs
+++ Understanding Ownership/References and Borrowing/The Last Revenge of Exclamations/tests/tests.rs
@@ -12,22 +12,18 @@ fn prints_hello_twice() {
     //TODO: recover from an incorrect output
     let actual_as_string = std::str::from_utf8(&actual).unwrap().trim().to_string();
     let lines: Vec<&str> = actual_as_string.split("\n").collect();
-    let expected_first_line = "hello is `Hello!`";
-    let expected_second_line = "hello is `Hello!!`";
+    let expected_first_line = "hello is 'Hello!'";
+    let expected_second_line = "hello is 'Hello!!'";
     assert!(
         lines.len() >= 2,
         "Make sure that the program prints at least two lines"
     );
     assert_eq!(
-        expected_first_line, lines[0],
-        "\nThe output is missing the first line with information about hello with '!' /
-        \nExpected: {}\nActual: {}\n",
-        expected_first_line, lines[0]
+        lines[0], expected_first_line,
+        "The output is missing the first line with information about hello with '!'"
     );
     assert_eq!(
-        expected_second_line, lines[1],
-        "\nThe output is missing the second line with information about hello with '!!' /
-        \nExpected: {}\nActual: {}\n",
-        expected_second_line, lines[1]
+        lines[1], expected_second_line,
+        "The output is missing the second line with information about hello with '!!'"
     );
 }
