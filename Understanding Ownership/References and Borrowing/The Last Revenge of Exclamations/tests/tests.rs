aa5038fe36fc96e937790d67316f87a064adc1f7
tito <tito@serpstat.com>
2023-05-05 22:08:37 +0300

    change error messages of the tests to be more explicit

--- Understanding Ownership/References and Borrowing/The Last Revenge of Exclamations/tests/tests.rs
+++ Understanding Ownership/References and Borrowing/The Last Revenge of Exclamations/tests/tests.rs
@@ -10,12 +10,24 @@ fn prints_hello_twice() {
         .unwrap()
         .stdout;
     //TODO: recover from an incorrect output
-    let actual_as_string = std::str::from_utf8(&actual).unwrap();
-    let expected_first_line  = "hello is `Hello!`\n";
-    let expected_second_line = "hello is `Hello!!`\n";
-    assert!(actual_as_string.contains(expected_first_line), "The output is missing the line with information about hello with '!'");
-    assert!(actual_as_string.contains(expected_second_line), "The output is missing the line with information about hello with \"!!\"");
+    let actual_as_string = std::str::from_utf8(&actual).unwrap().trim().to_string();
+    let lines: Vec<&str> = actual_as_string.split("\n").collect();
+    let expected_first_line = "hello is `Hello!`";
+    let expected_second_line = "hello is `Hello!!`";
+    assert!(
+        lines.len() >= 2,
+        "Make sure that the program prints at least two lines"
+    );
+    assert_eq!(
+        expected_first_line, lines[0],
+        "\nThe output is missing the first line with information about hello with '!' /
+        \nExpected: {}\nActual: {}\n",
+        expected_first_line, lines[0]
+    );
+    assert_eq!(
+        expected_second_line, lines[1],
+        "\nThe output is missing the second line with information about hello with '!!' /
+        \nExpected: {}\nActual: {}\n",
+        expected_second_line, lines[1]
+    );
 }
-
-
-
