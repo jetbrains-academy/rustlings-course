039b7c528a475fe8fd8edb1e7d367ca2f02c90ce
Vitaly Bragilevsky <bravit111@gmail.com>
2023-07-17 12:21:02 +0300

    fix #236: support check for introduce variable refactoring task

--- Common Programming Concepts/Variables/Introduce Variable Refactoring/src/main.rs
+++ Common Programming Concepts/Variables/Introduce Variable Refactoring/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let ten_years_ago = 1993 - 10;
-    let five_years_ago = 1993 - 5;
-    let one_year_ago = 1993 - 1;
+    let year = 1993;
+    let ten_years_ago = year - 10;
+    let five_years_ago = year - 5;
+    let one_year_ago = year - 1;
 
     println!("Ten years ago: {}", ten_years_ago);
     println!("Five years ago: {}", five_years_ago);
